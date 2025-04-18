f3c547ba5445edfe3f380e18269fb9ac3c234cb7
Nils Jaekel <nils.jaekel@redtronics.dev>
2025-04-18 23:04:11 +0200

    feat: add Gradle Kotlin DSL configuration for Java 21 and testing setup

---
+++ buildSrc/src/main/kotlin/huebflix-core.gradle.kts
@@ -0,0 +1,53 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    org.jetbrains.kotlin.jvm
+}
+
+repositories {
+    mavenCentral()
+}
+
+val targetJavaVersion: JavaVersion = JavaVersion.VERSION_21
+java {
+    targetCompatibility = targetJavaVersion
+    sourceCompatibility = targetJavaVersion
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(targetJavaVersion.majorVersion))
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+kotlin {
+    explicitApi()
+    compilerOptions {
+        allWarningsAsErrors = true
+    }
+    jvmToolchain(targetJavaVersion.majorVersion.toInt())
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        reports.junitXml.required = true
+        systemProperty("gradle.build.dir", layout.buildDirectory.get().asFile.absolutePath)
+
+        filter {
+            isFailOnNoMatchingTests = false
+        }
+
+        testLogging {
+            showExceptions = true
+            showStandardStreams = true
+            events = setOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
+}
