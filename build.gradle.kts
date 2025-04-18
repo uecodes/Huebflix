e23ee3da1fd685677fc52f1499c874a4ff68184a
Nils Jaekel <nils.jaekel@redtronics.dev>
2025-04-18 23:05:15 +0200

    refactor: simplify build.gradle.kts by removing unnecessary configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,3 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
-}
-
-group = "de.huebflix"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+    `huebflix-core`
 }
