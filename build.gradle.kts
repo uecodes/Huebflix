ce5ddfecd921b79c44436712df3524d753331cf4
Nils Jäkel <nils.jaekel@davils.net>
2025-04-20 00:15:50 +0200

    feat: add Ktor server dependencies and update versions in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,28 @@ plugins {
     `huebflix-core`
     `huebflix-publish`
 }
+
+dependencies {
+    // Ktor server
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.status.pages)
+    implementation(libs.ktor.server.cors)
+    implementation(libs.ktor.server.compression)
+    implementation(libs.ktor.server.rate.limit)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.resources)
+
+    // Ktor common
+    implementation(libs.ktor.common.kotlinx.serialization.json)
+
+    // Logging
+    implementation(libs.logback.classic)
+
+    // Testing
+    testImplementation(libs.ktor.server.test)
+    testImplementation(libs.kotest.assertion.core)
+    testImplementation(libs.kotest.property)
+    testImplementation(libs.kotest.runner.junit5)
+}
