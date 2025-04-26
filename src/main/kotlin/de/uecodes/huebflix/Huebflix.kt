4d07302a6c03188e20e5e39d340eb05788e9a5ba
Nils Jaekel <nils.jaekel@redtronics.dev>
2025-04-26 21:45:03 +0200

    feat: implement application routing and integrate OpenAPI with authentication

--- src/main/kotlin/de/uecodes/huebflix/Huebflix.kt
+++ src/main/kotlin/de/uecodes/huebflix/Huebflix.kt
@@ -1,10 +1,20 @@
 package de.uecodes.huebflix
 
+import de.uecodes.huebflix.routing.apiRouting
+import de.uecodes.huebflix.routing.auth.authProviders
+import de.uecodes.huebflix.routing.openApi
+import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
-
 fun main() {
-    val server = embeddedServer(Netty, port = 8080, host = "localhost") {}
+    val server = embeddedServer(Netty, port = 8080, host = "localhost", module = Application::huebflix)
     server.start(wait = true)
+}
+
+fun Application.huebflix() {
+    apiRouting("v1") {
+        openApi()
+        authProviders()
+    }
 }
